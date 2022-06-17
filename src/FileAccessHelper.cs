11268f4681b9fc7e08fa86ff8a56797c0ad77709
John Vitor Constant <johnvitorconstant@gmail.com>
2022-06-17 01:13:27 -0300

    Created generic respository

---
+++ src/FileAccessHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ExponentialGrowth
+{
+    public class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string fileName)
+        {
+            return System.IO.Path.Combine(FileSystem.AppDataDirectory, fileName);
+        }
+    }
+}
--- src/Pages/ExerciseForm.xaml.cs
+++ src/Pages/ExerciseForm.xaml.cs
@@ -1,9 +1,42 @@
+using ExponentialGrowth.Models;
+
 namespace ExponentialGrowth.Pages;
 
 public partial class ExerciseForm : ContentPage
 {
-	public ExerciseForm()
-	{
-		InitializeComponent();
-	}
+    public ExerciseForm()
+    {
+        InitializeComponent();
+    }
+
+    void btnGetOnClick(object sender, EventArgs e)
+    {
+
+        var exercises = App.ExerciseRepository.GetAll();
+
+        foreach (var exercise in exercises)
+        {
+            Console.WriteLine($"{exercise} \n");
+        }
+    }
+
+    void btnAddOnClick(object sender, EventArgs e)
+    {
+        try
+        {
+            var exercicio = new Exercise
+            {
+                Name = formName.Text,
+                Category = formCategory.Text,
+                MinimumLoad = Convert.ToDouble(formMinimumLoad.Text),
+                MaximumLoad = Convert.ToDouble(formMaximumLoad.Text),
+
+            };
+            App.ExerciseRepository.Add(exercicio);
+        }
+        catch
+        {
+            statusMessage.Text = "Dados inválidos";
+        }
+    }
 }
